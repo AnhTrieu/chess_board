3609a13f5e7df1ed57cbe5b67bd760601a47b103
Anh Trieu <anh.trieu@icloud.com>
2017-12-19 10:55:42 -0800

    second commit

--- script/move.js
+++ script/move.js
@@ -2,42 +2,24 @@ let blackPieces = document.querySelectorAll('.black')
 let whitePieces = document.querySelectorAll('.white')
 let pieces = []
 let selected = []
-let captured = []
 let sqrs = document.querySelectorAll('.chess-board .col-1')
 blackPieces.forEach(i => pieces.push(i))
 whitePieces.forEach(i => pieces.push(i))
 
-
-
 pieces.forEach(function(i) {
     i.addEventListener('click', function() {
-      if (selected.length === 1) {
-        // if (i.classList.contains(selected[0].classList.item(0))) {
-          selected.splice(0, 1)
-        //   console.log(selected);
-        // event.stopPropagation()
-        // }
-      }
-      if (selected.length === 0) {
         selected.splice(0, 0, i)
-      }
-    })
+    }, false)
 })
 
 sqrs.forEach(function(i) {
   i.addEventListener('click', function() {
     if (i.childNodes[1] === selected[0]) {
+      selected.splice(0, 1)
       event.stopPropagation()
     } else {
       if (i.children.length !== 0) {
-      //   captured.splice(0, 1)
-      //   captured.splice(0, 0, i.childNodes[1])
-      //
-      //   console.log(i.childNodes)
-
-        i.append(selected[0])
-        console.log(i.childNodes)
-        // (i.childNodes[1]).remove()
+        i.replaceChild(selected[0], i.childNodes[1])
         selected.splice(0, 1)
         selected.splice(1, 1)
       } else {
@@ -45,5 +27,5 @@ sqrs.forEach(function(i) {
         selected.splice(0, 1)
       }
     }
-  })
+  }, true)
 })
